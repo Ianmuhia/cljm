9b6991a0707511c69cdd1d67f305cd8ef39a3cd5
wisemiro <wycemiro2@gmail.com>
2022-02-03 22:06:32 +0300

    controllers

--- internal/controllers/jobs_controller.go
+++ internal/controllers/jobs_controller.go
@@ -1,5 +1,14 @@
 package controllers
 
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"maranatha_web/internal/models"
+	"maranatha_web/internal/utils/errors"
+	"net/http"
+	"strconv"
+)
+
 // import (
 // 	"log"
 // 	"net/http"
@@ -18,207 +27,207 @@ package controllers
 // 	"maranatha_web/utils/errors" //nolint:goimports
 // )
 
-// type GetAllJobsResponse struct {
-// 	Total int64               `json:"total"`
-// 	Jobs  []*models.ChurchJob `json:"jobs"`
-// }
+type GetAllJobsResponse struct {
+	Total int64               `json:"total"`
+	Jobs  []*models.ChurchJob `json:"jobs"`
+}
 
-// type CreateEventJobRequest struct {
-// 	Job     string `json:"job"`
-// 	EventId int    `json:"event_id"`
-// }
+type CreateEventJobRequest struct {
+	Job     string `json:"job"`
+	EventId int    `json:"event_id"`
+}
 
-// type UpdateEventJobRequest struct {
-// 	Job string `json:"job"`
-// }
+type UpdateEventJobRequest struct {
+	Job string `json:"job"`
+}
 
-// func GetAllEventJobs(ctx *gin.Context) {
-// 	id := ctx.Query("id")
-// 	value, _ := strconv.ParseInt(id, 10, 32)
-// 	if id == "" || value == 0 {
-// 		data := errors.NewBadRequestError("Provide an id to the request.Id cannot be zero")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
-// 	}
-// 	i, err := strconv.ParseUint(id, 10, 32)
-// 	if err != nil {
-// 		data := errors.NewBadRequestError("Provide an id to the request.")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
+func (r *Repository) GetAllEventJobs(ctx *gin.Context) {
+	id := ctx.Query("id")
+	value, _ := strconv.ParseInt(id, 10, 32)
+	if id == "" || value == 0 {
+		data := errors.NewBadRequestError("Provide an id to the request.Id cannot be zero")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
+	}
+	i, err := strconv.ParseUint(id, 10, 32)
+	if err != nil {
+		data := errors.NewBadRequestError("Provide an id to the request.")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
 
-// 	}
+	}
 
-// 	jobs, count, errr := services.JobsService.GetAllEventJobs(uint(i))
-// 	if errr != nil {
-// 		data := errors.NewBadRequestError("Error Processing request")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
-// 	}
+	jobs, count, errr := r.jobService.GetAllEventJobs(uint(i))
+	if errr != nil {
+		data := errors.NewBadRequestError("Error Processing request")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
+	}
 
-// 	data := GetAllJobsResponse{
-// 		Total: count,
-// 		Jobs:  jobs,
-// 	}
-// 	ctx.JSON(http.StatusOK, data)
+	data := GetAllJobsResponse{
+		Total: count,
+		Jobs:  jobs,
+	}
+	ctx.JSON(http.StatusOK, data)
 
-// }
+}
 
-// func GetJobByEvent(ctx *gin.Context) {
+func (r *Repository) GetJobByEvent(ctx *gin.Context) {
 
-// 	eventId := ctx.Query("event_id")
-// 	jobId := ctx.Query("job_id")
+	eventId := ctx.Query("event_id")
+	jobId := ctx.Query("job_id")
 
-// 	value, _ := strconv.ParseInt(eventId, 10, 32)
-// 	if eventId == "" || value == 0 {
-// 		data := errors.NewBadRequestError("Provide an id to the request.Id cannot be zero")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
-// 	}
-// 	i, err := strconv.ParseUint(eventId, 10, 32)
-// 	if err != nil {
-// 		data := errors.NewBadRequestError("Provide an id to the request.")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
+	value, _ := strconv.ParseInt(eventId, 10, 32)
+	if eventId == "" || value == 0 {
+		data := errors.NewBadRequestError("Provide an id to the request.Id cannot be zero")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
+	}
+	i, err := strconv.ParseUint(eventId, 10, 32)
+	if err != nil {
+		data := errors.NewBadRequestError("Provide an id to the request.")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
 
-// 	}
+	}
 
-// 	value2, _ := strconv.ParseInt(jobId, 10, 32)
-// 	if jobId == "" || value2 == 0 {
-// 		data := errors.NewBadRequestError("Provide an id to the request.Id cannot be zero")
-// 		log.Println("herere")
+	value2, _ := strconv.ParseInt(jobId, 10, 32)
+	if jobId == "" || value2 == 0 {
+		data := errors.NewBadRequestError("Provide an id to the request.Id cannot be zero")
+		log.Println("herere")
 
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
-// 	}
-// 	i2, err := strconv.ParseUint(jobId, 10, 32)
-// 	if err != nil {
-// 		data := errors.NewBadRequestError("Provide an id to the request.")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
+	}
+	i2, err := strconv.ParseUint(jobId, 10, 32)
+	if err != nil {
+		data := errors.NewBadRequestError("Provide an id to the request.")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
 
-// 	}
+	}
 
-// 	jobs, errr := services.JobsService.GetJobByEvent(uint(i), uint(i2))
-// 	if errr != nil {
-// 		data := errors.NewBadRequestError("Error Processing request")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
-// 	}
+	jobs, errr := r.jobService.GetJobByEvent(uint(i), uint(i2))
+	if errr != nil {
+		data := errors.NewBadRequestError("Error Processing request")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
+	}
 
-// 	ctx.JSON(http.StatusOK, jobs)
+	ctx.JSON(http.StatusOK, jobs)
 
-// }
+}
 
-// func CreateEventJob(ctx *gin.Context) {
-// 	var req CreateEventJobRequest
+func (r *Repository) CreateEventJob(ctx *gin.Context) {
+	var req CreateEventJobRequest
 
-// 	if err := ctx.ShouldBindJSON(&req); err != nil {
-// 		restErr := errors.NewBadRequestError("invalid json body")
-// 		ctx.JSON(restErr.Status, restErr)
-// 		ctx.Abort()
-// 		return
-// 	}
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		ctx.JSON(restErr.Status, restErr)
+		ctx.Abort()
+		return
+	}
 
-// 	event, err := services.EventsService.GetSingleEvent(uint(req.EventId))
-// 	if err != nil {
-// 		restErr := errors.NewBadRequestError("Error processing the request .Could not get the event by that id.")
-// 		ctx.JSON(restErr.Status, restErr)
-// 		ctx.Abort()
-// 		return
-// 	}
+	event, err := r.eventsService.GetSingleEvent(uint(req.EventId))
+	if err != nil {
+		restErr := errors.NewBadRequestError("Error processing the request .Could not get the event by that id.")
+		ctx.JSON(restErr.Status, restErr)
+		ctx.Abort()
+		return
+	}
 
-// 	data := models.ChurchJob{
-// 		Duty:          req.Job,
-// 		ChurchEventID: event.ID,
-// 	}
+	data := models.ChurchJob{
+		Duty:          req.Job,
+		ChurchEventID: event.ID,
+	}
 
-// 	job, errr := services.JobsService.CreateEventJob(data)
-// 	if errr != nil {
-// 		restErr := errors.NewBadRequestError("Error creating the job.")
-// 		ctx.JSON(restErr.Status, restErr)
-// 		ctx.Abort()
-// 		return
-// 	}
-// 	ctx.JSON(http.StatusCreated, job)
+	job, errr := r.jobService.CreateEventJob(data)
+	if errr != nil {
+		restErr := errors.NewBadRequestError("Error creating the job.")
+		ctx.JSON(restErr.Status, restErr)
+		ctx.Abort()
+		return
+	}
+	ctx.JSON(http.StatusCreated, job)
 
-// }
+}
 
-// func DeleteJob(ctx *gin.Context) {
-// 	id := ctx.Query("id")
-// 	value, _ := strconv.ParseInt(id, 10, 32)
-// 	if id == "" || value == 0 {
-// 		data := errors.NewBadRequestError("Provide an id to the request.Id cannot be zero")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
-// 	}
-// 	i, err := strconv.ParseUint(id, 10, 32)
-// 	if err != nil {
-// 		data := errors.NewBadRequestError("Provide an id to the request.")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
+func (r *Repository) DeleteJob(ctx *gin.Context) {
+	id := ctx.Query("id")
+	value, _ := strconv.ParseInt(id, 10, 32)
+	if id == "" || value == 0 {
+		data := errors.NewBadRequestError("Provide an id to the request.Id cannot be zero")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
+	}
+	i, err := strconv.ParseUint(id, 10, 32)
+	if err != nil {
+		data := errors.NewBadRequestError("Provide an id to the request.")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
 
-// 	}
-// 	errr := services.JobsService.DeleteJob(uint(i))
-// 	if errr != nil {
-// 		data := errors.NewBadRequestError("Error Processing request")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
-// 	}
+	}
+	errr := r.jobService.DeleteJob(uint(i))
+	if errr != nil {
+		data := errors.NewBadRequestError("Error Processing request")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
+	}
 
-// 	ctx.JSON(http.StatusOK, gin.H{
-// 		"Message": "Successfully deleted job",
-// 	})
+	ctx.JSON(http.StatusOK, gin.H{
+		"Message": "Successfully deleted job",
+	})
 
-// }
-// func UpdateJob(ctx *gin.Context) {
-// 	id := ctx.Query("id")
-// 	value, _ := strconv.ParseInt(id, 10, 32)
-// 	if id == "" || value == 0 {
-// 		data := errors.NewBadRequestError("Provide an id to the request.Id cannot be zero")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
-// 	}
-// 	i, err := strconv.ParseUint(id, 10, 32)
-// 	if err != nil {
-// 		data := errors.NewBadRequestError("Provide an id to the request.")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
+}
+func (r *Repository) UpdateJob(ctx *gin.Context) {
+	id := ctx.Query("id")
+	value, _ := strconv.ParseInt(id, 10, 32)
+	if id == "" || value == 0 {
+		data := errors.NewBadRequestError("Provide an id to the request.Id cannot be zero")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
+	}
+	i, err := strconv.ParseUint(id, 10, 32)
+	if err != nil {
+		data := errors.NewBadRequestError("Provide an id to the request.")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
 
-// 	}
-// 	var req UpdateEventJobRequest
-// 	if err := ctx.ShouldBindJSON(&req); err != nil {
-// 		restErr := errors.NewBadRequestError("invalid json body")
-// 		ctx.JSON(restErr.Status, restErr)
-// 		ctx.Abort()
-// 		return
-// 	}
-// 	data := models.ChurchJob{
-// 		Duty: req.Job,
-// 	}
+	}
+	var req UpdateEventJobRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		ctx.JSON(restErr.Status, restErr)
+		ctx.Abort()
+		return
+	}
+	data := models.ChurchJob{
+		Duty: req.Job,
+	}
 
-// 	errr := services.JobsService.UpdateJob(uint(i), data)
-// 	if errr != nil {
-// 		data := errors.NewBadRequestError("Error Processing request")
-// 		ctx.JSON(data.Status, data)
-// 		ctx.Abort()
-// 		return
-// 	}
+	errr := r.jobService.UpdateJob(uint(i), data)
+	if errr != nil {
+		data := errors.NewBadRequestError("Error Processing request")
+		ctx.JSON(data.Status, data)
+		ctx.Abort()
+		return
+	}
 
-// 	ctx.JSON(http.StatusOK, gin.H{
-// 		"Message": "Successfully updated job",
-// 	})
+	ctx.JSON(http.StatusOK, gin.H{
+		"Message": "Successfully updated job",
+	})
 
-// }
+}
